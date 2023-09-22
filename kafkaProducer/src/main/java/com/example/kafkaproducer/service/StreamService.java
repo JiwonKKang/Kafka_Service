174550cd02daaeb73fd45c96850c456ebc214f85
강지원 <kangji149@naver.com>
2023-09-22 16:24:54 +0900

    feat: KafkaStream Join

--- kafkaProducer/src/main/java/com/example/kafkaproducer/service/StreamService.java
+++ kafkaProducer/src/main/java/com/example/kafkaproducer/service/StreamService.java
@@ -6,10 +6,13 @@ import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.JoinWindows;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Printed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
@@ -20,12 +23,18 @@ public class StreamService {
 
     @Autowired
     public void buildPipeline(StreamsBuilder sb) {
-        KStream<String, String> myStream = sb.stream("fastCampus", Consumed.with(STRING_SERDE, STRING_SERDE));
+        KStream<String, String> leftStream = sb.stream("leftTopic", Consumed.with(STRING_SERDE, STRING_SERDE));
+        KStream<String, String> rightStream = sb.stream("rightTopic", Consumed.with(STRING_SERDE, STRING_SERDE));
         //일단 consume을 한뒤에 받은 데이터를 프로세싱하여 다른토픽에 Publish
-        myStream.print(Printed.toSysOut());
-        myStream.filter((key, value) -> value.contains("freeClass")).to("freeClassList");
+        leftStream.print(Printed.toSysOut());
+        rightStream.print(Printed.toSysOut());
 
+        KStream<String, String> joinedStream = leftStream.join(
+                rightStream,
+                (leftValue, rightValue) -> leftValue + ":" + rightValue,
+                JoinWindows.ofTimeDifferenceWithNoGrace(Duration.ofMinutes(1)));
 
+        joinedStream.print(Printed.toSysOut());
     }
 
 }
